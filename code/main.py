7bf35cfef0b1e3d6a14c0814ee609ec3fdf9bdf5
Elias E. G. Riise <eliasriise@outlook.com>
2022-09-02 16:13:08 +0200

    code initilaized

---
+++ code/main.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+# from tf.keras import Model
+
+
+
+
+def main():
+    print("hello world")
+    m = tf.keras.models.Sequential()
+    print(m)
+if __name__ == "__main__":
+    main()
