45faf6837c0503a0b11e2129978886ee6410fe87
Elias E. G. Riise <eliasriise@outlook.com>
2022-09-22 18:01:32 +0200

    rm http request way to get data

--- code/data.py
+++ code/data.py
@@ -15,37 +15,8 @@ from os import getcwd
 
 from prints import printw, printe, printo, printc
 
+coco_file_path = getcwd() + '/../LIACi_dataset_pretty/coco-labels.json'
 
-def retrieve_data():
-    # [?iterationId][&tagIds][&orderBy][&take][&skip]
-
-    
-    # endpoint = 'southcentralus.api.cognitiveservies.azure.com'
-    endpoint = 'southcentralus.api.cognitive.microsoft.com'
-    url = get_url(endpoint, ic_api)
-    payload = {
-        # 'iterationId': None, 
-        'tagIds': 'd01c9230-3ee1-4a88-8b9a-6bc7211b7ff8',
-        # 'orderBy': None,
-        # 'take': None,
-        # 'skip': 0
-        }
-    headers = {"Training-key":  api_key}
-
-    r = get(
-        headers=headers, 
-        params=payload,
-        # auth=('apikey', api_key),
-        url=url 
-        )
-    
-    printw(r.url)
-    print(r.status_code)
-    print(r.content)
-    
-    image = r.content
-    
-    return image
 
 def load_im(file_name):
     path = getcwd() + '/../LIACi_dataset_pretty/images/' + file_name
@@ -57,21 +28,27 @@ def load_im(file_name):
 
     return im
 
+def get_cats(path=coco_file_path):
+    c = COCO(path)
+    return c.getCatIds()
+
 def get_hot_vec(ann_objs, n_cats):
     """ returns a multi-label hot vector """
+
     hv = np.zeros(n_cats)
+
     for a_obj in ann_objs:
         a = a_obj['category_id']
         # hv[a-1] += 1 # Should supervision be done like this?
         hv[a-1] = 1
         
-def load_from_coco(coco_file, n_imgs=1):
+def load_from_coco(n_imgs=1, coco_file=coco_file_path):
 
     c = COCO(coco_file)
     idxs = list(range(1, n_imgs + 1))
     images = []
     anns = []
-    n_cats = len(c.getCatIds())
+    n_cats = len(get_cats())
 
     for idx in idxs:
         im_obj = c.loadImgs(idx)[0]
@@ -87,13 +64,6 @@ def load_from_coco(coco_file, n_imgs=1):
     images = np.array(images)
     
     return images, anns
-
-def get_data():
-    coco_file_path = getcwd() + '/../LIACi_dataset_pretty/coco-labels.json'
-
-    X, Y = load_from_coco(coco_file_path, n_imgs=4)
-
-    return X, Y
 
 if __name__ == "__main__":
     get_data()
