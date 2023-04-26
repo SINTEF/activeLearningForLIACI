ad7e3f88b76bbf78f100dcde6c869c75651b6a42
Elias Riise <eliasriise@outlook.com>
2023-04-26 09:19:11 +0000

    uncertainty measurement should be done now

--- src/plot_uncertainty.py
+++ src/plot_uncertainty.py
@@ -4,10 +4,9 @@ import scipy.stats as stats
 
 from PIL import Image
 from data import get_cat_lab
-import config as cnf
+import utils.config as cnf
 from prints import printc
-from utils import axs_legend
-# import itertool
+from utils.plt import axs_legend, save_fig
 
 
 labels = get_cat_lab()
@@ -15,15 +14,15 @@ labels = get_cat_lab()
 
 def get_values():
     with open('npy/'+'u.npy', 'rb') as f:
+        printc('Loaded:')
         values = np.load(f)
+        printc(f'Values: {values.shape}')
         b_preds = np.load(f)
+        printc(f'b_preds: {b_preds.shape}')
         preds = np.load(f)
+        printc(f'preds: {preds.shape}')
         images = np.load(f)
-    printc('Loaded:')
-    printc(f'Values: {values.shape}')
-    printc(f'b_preds: {b_preds.shape}')
-    printc(f'preds: {preds.shape}')
-    printc(f'images: {images.shape}')
+        printc(f'images: {images.shape}')
     return values, b_preds, preds, images
 
 # Plots image true label mus/vars/sigs
@@ -39,29 +38,34 @@ def plot_im_tl_mvs(sample_data, b_preds, axs):
     
  
     x = np.arange(mus.shape[0])
-    true_labels = np.flatnonzero(b_preds)
+    annotates_labels = np.flatnonzero(b_preds)
 
-    for tl in true_labels:
-        axs[0].plot(x, mus[:,tl], label=f'{labels[tl]} $\mu$')
-        axs[1].plot(x, vars[:,tl], label=f'{labels[tl]} $\sigma^2$')
-        axs[2].plot(x, sigs[:,tl], label=f'{labels[tl]} $\sigma$')
+    for al in annotates_labels:
+        axs[0].plot(x, vars[:,al], label=f'{labels[al]} $\sigma^2$')
+        axs[1].plot(x, sigs[:,al], label=f'{labels[al]} $\sigma$')
+        axs[2].plot(x, mus[:,al], label=f'{labels[al]} $\mu$')
 
 
-def plot_im_tl_mvs_create(sample_data, b_preds):
+def plot_im_tl_mvs_create(sample_data, b_preds, path_to_dir):
     fig, axs = plt.subplots(3)
     
     plot_im_tl_mvs(sample_data, b_preds, axs)
     
     axs_legend(axs)
 
-    axs[0].set_xlabel('$\mu$ progression')
-    axs[1].set_xlabel('$\sigma^2$ progression')
-    axs[2].set_xlabel('$\sigma$ progression')
+    axs[0].set_ylabel('$\sigma^2$-value')
+    axs[1].set_ylabel('$\sigma$-value')
+    axs[2].set_ylabel('$\mu$-value')
+
+    axs[0].set_xlabel('$N$ samples used to calculate $\sigma^2$')
+    axs[1].set_xlabel('$N$ samples used to calculate $\sigma$')
+    axs[2].set_xlabel('$N$ samples used to calculate $\mu$')
 
     fig.set_figheight(10)
-    fig.suptitle('$\mu$, $\sigma^2$ and $\sigma$ progression throughout all samples')
+    fig.suptitle('$\sigma^2$, $\sigma$, & $\mu$  progression throughout all samples')
     fig.tight_layout()
-    fig.savefig('../out_imgs/uncertainty/'+ 'mu_var_change.png')
+    save_fig(path='../out_imgs/uncertainty/'+path_to_dir, file_name='mu_var_change', fig=fig)
+    
 
 def plot_im_tl_pdf(sample_data, b_preds, preds, axs):
     mus = np.mean(sample_data, axis=0)
@@ -71,45 +75,55 @@ def plot_im_tl_pdf(sample_data, b_preds, preds, axs):
     annotated_labels = np.flatnonzero(b_preds)
     axs.axvline(cnf.threshold, linestyle='dashed', linewidth=1,c='red', label='Threshold')
 
-    for al in annotated_labels:
+    for al in np.flip(annotated_labels):
         mu = mus[al]
         var = vars[al]
         std = stds[al]
         label = labels[al]
         pred = preds[al]
+        # if var.round(4) == 0:
+        #     continue # var too small to plot
 
         x = np.linspace(mu-3*std, mu+3*std)
         axs.plot(x, stats.norm.pdf(x, mu, std), label=f'Class {label}, $\sigma^2={np.round(var, 4)}$')
         color = axs.lines[-1].get_color() # get color from last plot
         axs.scatter(mu-2*std, stats.norm.pdf(mu-2*std, mu,std), c=color, marker='X')
-        axs.axvline(pred, c=color, linestyle='dashdot')
+        axs.axvline(pred, c=color, linestyle='dashdot', label=f'Original class output value')
         # axs.scatter(sample_data[:,al], stats.norm.pdf(sample_data[:,al], mu, std))
 
-def plot_im_tl_pdf_create(sample_data, b_preds, preds, image):
+def plot_im_tl_pdf_create(sample_data, b_preds, preds, image, path_to_dir):
     fig, axs = plt.subplots(1)
     plot_im_tl_pdf(sample_data, b_preds, preds, axs)
                 
     axs.legend()
 
     axs.set_xlim([0,1])
-    
-    fig.suptitle('PDF for the annotated labels')
+    axs.set_ylim(bottom=0)
+    axs.set_xlabel('Output neruon value after sigmoid activation')
+    fig.suptitle('Probability density function for the annotated labels')
     fig.tight_layout()
-    fig.savefig('../out_imgs/uncertainty/'+ 'pdf_dist_1.png')
+    save_fig('../out_imgs/uncertainty/'+ path_to_dir, 'pdf_dist', fig)
+
     im = Image.fromarray(image)
-    im.save('../out_imgs/uncertainty/'+ 'image_1.png')
+    im.save('../out_imgs/uncertainty/'+ path_to_dir+ 'image.png')
 
 
 
 def main():
     sample_datas, b_preds, preds, images = get_values() # sample_datas (n_im, n_samp, n_lab)
-    frame = 3
+    for frame, (sd, bp, p, im) in enumerate(zip(sample_datas, b_preds, preds, images)):
+        path_to_dir = 'im'+str(frame)+'/'
+        plot_im_tl_mvs_create(sd, bp, path_to_dir)
+        plot_im_tl_pdf_create(sd[:cnf.n_samples], bp, p, im, path_to_dir)
+        # plot_im_tl_pdf_create(sample_datas[frame,:cnf.n_samples], b_preds[frame,:cnf.n_samples], preds[frame,:cnf.n_samples], images[frame,:cnf.n_samples])
+
+
+
     if not frame < images.shape[0]:
         print(f'frame needs to be within images, was {frame} and {images.shape[0]}')
         exit()
-    plot_im_tl_mvs_create(sample_datas[frame],b_preds[frame])
-
-    plot_im_tl_pdf_create(sample_datas[frame,:cnf.n_samples], b_preds[frame,:cnf.n_samples], preds[frame,:cnf.n_samples], images[frame,:cnf.n_samples])
+    # for 
+    
     
 
     
--- src/uncertainty.py
+++ src/uncertainty.py
@@ -7,11 +7,14 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 
-import config as cnf
+import utils.config as cnf
 from prints import printc
-from model import hullifier_load, hullifier_compile
-from utils import get_dict_from_file, get_predictions_bool
 from data import load_from_coco, shuffle_data, split_data, get_cat_lab
+from model import hullifier_clone
+from utils.model import hullifier_load, hullifier_compile, get_lr, classifier_create, preproc_model_create
+from utils.file import get_dict_from_file 
+from utils.utils import get_predictions_bool
+from utils.uncertainty import find_uncertainty, get_feature_extractor
 
 measure_time = True
 if measure_time:
@@ -37,25 +40,40 @@ def t():
 # [] 
 # [] 
 
+def get_classifier(model):
+    lr = get_lr(model)
 
-def measure_uncertainty(model, X, Y, n_samples):
-    labels = get_cat_lab()
+    # Create classifier
+    classifier = classifier_create(dr_rate=0.1)
+    hullifier_compile(classifier, lr) # Compile
+    classifier.build((None, 7, 7, 1024)) # and Build
+    classifier.set_weights(model.get_weights()[-len(classifier.get_weights()):]) # Set current models weights
+    return classifier
+
+
+def measure_uncertainty(model, X, n_samples):
+    pre_proc = preproc_model_create()
+    fe = get_feature_extractor(model)
+    classifier = get_classifier(model)
+    # labels = get_cat_lab()
     b_preds, preds = get_predictions_bool(model, X)
 
-    sample_data = np.empty((X.shape[0], n_samples, Y.shape[1])) # shape (n_images, n_samples, 9)
+    uncertainties = np.empty((X.shape[0], n_samples, 9)) # shape (n_images, n_samples, 9)
     
     for i, im in enumerate(X):
         im = expand_dims(convert_to_tensor(im), 0)
+        im = pre_proc(im)
+        features = fe(im,training=False)
         for k in range(n_samples):
-            pred = model(im, training=True)[0]
-            sample_data[i, k] = pred
+            pred = classifier(features, training=True)[0]
+            uncertainties[i, k] = pred
     
 
     # im = Image.fromarray(X[frame])
     # im.save('../out_imgs/uncertainty/'+ 'image_1.png')
     
     with open('npy/'+'u.npy', 'wb') as f:
-        np.save(f, sample_data)
+        np.save(f, uncertainties)
         np.save(f, b_preds)
         np.save(f, preds)
         np.save(f, X) # Save images sample data comes from
@@ -66,13 +84,8 @@ def main():
     params = get_dict_from_file(cnf.model_path + 'params.txt')
     # load old model
     hullifier = hullifier_load(cnf.model_path)
-    # Set dropout layer to a new value
-    hullifier.layers[-1].layers[1].rate = 0.2
-    # New cloned model with fresh weights
-    model = clone_model(hullifier)
-    hullifier_compile(model, float(params.lr))
-    # copy weights from old model to new
-    model.set_weights(hullifier.get_weights())
+    # New cloned model with same weights
+    model = hullifier_clone(hullifier)
 
     ## Now find uncertainty
     
@@ -82,11 +95,11 @@ def main():
     X,Y,XT,YT = split_data(X,Y,float(params.v_split))
 
     # Don't set to < 2
-    n_unc_im = 5
+    n_unc_im = 4
     # n_samples = cnf.n_samples
     n_samples = 350
 
-    measure_uncertainty(model, XT[:n_unc_im], YT[:n_unc_im], n_samples)
+    measure_uncertainty(model, XT[51:51+n_unc_im], n_samples)
 
 
 
