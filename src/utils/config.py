457a6abdf82d996e0a42d7554c69b0ca8f47a670
Elias Riise <eliasriise@outlook.com>
2023-05-24 08:02:44 +0000

    update value

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,7 +5,7 @@ curr_bench = '2022_12_07_1709'
 model_path = '../benchmarks/' + curr_bench + '/epochs-30_batchsize-50_lr-0.0004_vsplit-0.1_v2-False/model/'
 new_images_dir = '../user_annotations/'
 
-threshold = 0.35
+threshold = 0.38
 fraction = 0.3
 wiggle_room = 0.06
 n_samples = 100
