15ba32113280a3937f195b9e252311b4200e7921
Elias Riise <eliasriise@outlook.com>
2023-05-26 11:00:04 +0000

    update f1plot with new labels

--- src/plot_f1.py
+++ src/plot_f1.py
@@ -9,7 +9,7 @@ from utils.plt import save_fig
 
 base_dir = '../out_imgs/f1/'
 
-def plot_f1_scores(path, model_names, m_preds, xt, yt):
+def plot_f1_scores(path, model_names, m_preds, yt):
     threshs = np.arange(0.01, 1.0, 0.01) # Start, stop, step
 
     m_f1_scrs = []
@@ -37,13 +37,13 @@ def plot_f1_scores(path, model_names, m_preds, xt, yt):
 
         best = np.argmax(f1_scrs)
 
-        axs[0].plot(threshs, f1_scrs, label=mn+' F1 score graph')
+        axs[0].plot(threshs, f1_scrs, label=mn)
         color = axs[0].lines[-1].get_color() # get color from last plot
-        axs[0].axhline(f1_scrs[best], label=mn+f' F1 score {f1_scrs[best]}', linestyle='dashed', c=color)
+        axs[0].axhline(f1_scrs[best], label=f'Best $F1$ {f1_scrs[best]}', linestyle='dashed', c=color)
         axs[0].axvline(threshs[best], linestyle='dashed', c=color)
 
-        axs[1].plot(ps, rs, label='Precision Recall Curve')
-        axs[1].scatter(ps[best],rs[best], label='Best $F1\ score$')
+        axs[1].plot(ps, rs, label=mn)
+        axs[1].scatter(ps[best],rs[best], label='Best $F1$ ')
 
     # plt.rcParams.update({'font.size': 20})
     axs[0].set_ylabel('F1 score')
@@ -62,18 +62,18 @@ def plot_f1_scores(path, model_names, m_preds, xt, yt):
 
     old_height = fig.get_figheight()
     fig.set_figheight(4)
-    fig.suptitle(f'F1 score evaluation'+ (' for models with a budget' if path =='budget/' else '')+',\nmcu=Monte Carlo uncertain, mcc=Monte Carlo non-uncertain, tiu=Threshold interval uncertain, tic=Threshold interval non-uncertain', wrap=True)
+    fig.suptitle(f'F1 score evaluation'+ (' for models with a budget' if path =='budget/' else '')+',\nMC Unc=Monte Carlo uncertain, MC Crt=Monte Carlo non-uncertain, TI Unc=Threshold interval uncertain, TI Crt=Threshold interval non-uncertain', wrap=True)
     save_fig(base_dir+path,'f1_ev', fig)
     fig.set_figheight(old_height)
 
     return fig, axs
 
 def plot_version(version, X, Y):
-    x,y,xt,yt = split_data(X,Y, 0.1)
-    mcu = 'mcu'
-    mcc = 'mcc'
-    tic = 'tic'
-    tiu = 'tiu'
+    _,_,xt,yt = split_data(X,Y, 0.1)
+    mcu = 'MC Unc'
+    mcc = 'MC Crt'
+    tiu = 'TI Crt'
+    tic = 'TI Unc'
     
 
     model_names = [
@@ -82,13 +82,12 @@ def plot_version(version, X, Y):
         tiu,
         tic
     ]
-
-    b_models = [hullifier_load('models/iuc_models/'+version+mn) for mn in model_names] # load all models
     
+    b_models = [hullifier_load('models/iuc_models/'+version+mn.replace(" ","")[:3].lower()) for mn in model_names] # load all models
     m_b_preds = [ model.predict(xt) for model in b_models ] # all models predict on test data
     
     
-    return plot_f1_scores(version, model_names, m_b_preds, xt, yt)
+    return plot_f1_scores(version, model_names, m_b_preds, yt)
 
 def main():
     X, Y = load_from_coco()
